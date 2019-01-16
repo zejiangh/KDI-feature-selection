c39b1b025859dfa1a8598c39362fd1e84debc457
zejiangh <46656761+zejiangh@users.noreply.github.com>
2019-01-16 17:56:53 -0500

    Add files via upload

--- example.py
+++ example.py
@@ -1,17 +1,19 @@
 from __future__ import print_function
 import numpy as np
 import sys
-import DI, CCM
+import DI
 import scipy
 import scipy.io as sio
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
+import matplotlib.pyplot as plt
     
 svm_tuned_params = [{'kernel': ['rbf'], 'gamma': [1e0,1e-1,1e-2,1e-3,1e-4], 'C': [1, 10, 100]}]
 svc = SVC(kernel='rbf')
     
-data = sio.loadmat('SMK-CAN-187.mat')
+data = sio.loadmat('SMK_CAN_187.mat')
+# data = sio.loadmat('TOX_171.mat')
 x_train = data['X']
 y_train = data['Y']
 print(x_train.shape, y_train.shape)
@@ -20,11 +22,12 @@ scaler = MinMaxScaler()
 x_train = scaler.fit_transform(x_train)
 
 epsilon = 1e-3; mu = 0.001; num_features = 100; type_Y = 'categorical'
-rank_di = DI.di(x_train, y_train, num_features, type_Y, epsilon, mu, learning_rate = 0.1, iterations = 500, verbose = True)
+rank_di = DI.di(x_train, y_train, num_features, type_Y, epsilon, mu, learning_rate = 0.1, iterations = 1000, verbose = True)
 
 num = [100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5]
 
 print("DI SCORES**********")
+res = []
 for i in range (20):
     selected_feats_di = np.argsort(rank_di)[:num[i]]
     x_train_selected_di = np.take(x_train, selected_feats_di, axis=1)
@@ -35,7 +38,14 @@ for i in range (20):
         outer_cv = KFold(n_splits=5, shuffle=True, random_state=rs)
         clf = GridSearchCV(estimator=svc, param_grid=svm_tuned_params, cv=inner_cv)
         score_di += cross_val_score(clf, X=x_train_selected_di, y=y_train, cv=outer_cv)
-    print(score_di.mean()/5)
+    res.append(score_di.mean()/5)
+    
+plt.plot(num, res, marker='+')
+plt.ylabel('KSVM Accuracy')
+plt.xlabel('# features selected')
+plt.title('KDI feature selection on SMK-CAN-187 data')
+plt.grid(True)
+plt.show()
 
 print("full feature**********")
 score_full = 0
@@ -44,4 +54,4 @@ for rs in range (5):
     outer_cv = KFold(n_splits=5, shuffle=True, random_state=rs)
     clf = GridSearchCV(estimator=svc, param_grid=svm_tuned_params, cv=inner_cv)
     score_full += cross_val_score(clf, X=x_train, y=y_train, cv=outer_cv)
-print(score_full.mean()/5)
+print('Full feature accuracy:',score_full.mean()/5)
